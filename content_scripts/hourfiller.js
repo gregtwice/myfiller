9eda71c7dfef985dd7205a425082922d6c995f54
Grégoire DEFOY <g.defoy@technologyandstrategy.com>
2025-06-04 15:02:53 +0200

    set project hours

--- content_scripts/hourfiller.js
+++ content_scripts/hourfiller.js
@@ -1,71 +1,81 @@
 (function () {
-    if (window.hasMyFireFillerRun) {
-        return;
-    }
-    window.hasMyFireFillerRun = true;
+  if (window.hasMyFireFillerRun) {
+    return;
+  }
+  window.hasMyFireFillerRun = true;
 
-    function mf_is_visible(elem) {
-        return elem.style.display != "none"
-    }
+  function mf_is_visible(elem) {
+    return elem.style.display != "none";
+  }
 
-
-    function my_fire_text_field_filler(cssClass, hour, base = document) {
-        let event = new Event("blur");
-        let items = Array.from(base.querySelectorAll(cssClass));
-        items.filter(mf_is_visible).forEach((node) => {
-            node.firstElementChild.value = hour;
-            // Make the mf js update the hours worked 
-            node.firstElementChild.dispatchEvent(event);
-        });
-    }
-
-    function my_fire_set_hour_of_arrival(hour) {
-        my_fire_text_field_filler(".txtStartDay", hour);
-    }
-
-    function my_fire_set_hour_of_departure(hour) {
-        my_fire_text_field_filler(".txtEndDay", hour)
-    }
-
-    function my_fire_set_pause_duration(hour) {
-        my_fire_text_field_filler(".txtPauseDay", hour)
-    }
-
-    function my_fire_set_project_hours(projectIndex, hoursWorked) {
-        let project = document.querySelectorAll(".affectation").item(projectIndex);
-        my_fire_text_field_filler(".heureProjet", hoursWorked, project);
-
-    }
-
-    /**
-     * Reset all the fields to their default values
-     */
-    function reset_hour_fields() {
-        my_fire_set_hour_of_arrival("00:00");
-        my_fire_set_pause_duration("00:45");
-        my_fire_set_hour_of_departure("00:00");
-        for (let project = 0; project < get_number_of_projects(); project++) {
-            my_fire_set_project_hours(project + 1, "00:00");
-        }
-    }
-
-    /**
-     * Calculates the amount of projects - minus the IC
-     */
-    function get_number_of_projects() {
-        return document.querySelectorAll(".affectation").length - 1
-    }
-
-
-    browser.runtime.onMessage.addListener((message) => {
-        console.log(message);
-
-        if (message.command === "reset") {
-            reset_hour_fields()
-        }
-        if (message.command === "fill") {
-            my_fire_set_hour_of_arrival(message.startOfDay);
-        }
+  function my_fire_text_field_filler(cssClass, hour, base = document) {
+    let event = new Event("blur");
+    let items = Array.from(base.querySelectorAll(cssClass));
+    items.filter(mf_is_visible).forEach((node) => {
+      node.firstElementChild.value = hour;
+      // Make the mf js update the hours worked
+      node.firstElementChild.dispatchEvent(event);
     });
+  }
 
+  function my_fire_set_hour_of_arrival(hour) {
+    my_fire_text_field_filler(".txtStartDay", hour);
+  }
+
+  function my_fire_set_hour_of_departure(hour) {
+    my_fire_text_field_filler(".txtEndDay", hour);
+  }
+
+  function my_fire_set_pause_duration(hour) {
+    my_fire_text_field_filler(".txtPauseDay", hour);
+  }
+
+  function my_fire_set_project_hours(projectIndex, hoursWorked) {
+    let project = document.querySelectorAll(".affectation").item(projectIndex);
+    my_fire_text_field_filler(".heureProjet", hoursWorked, project);
+  }
+
+  /**
+   * Reset all the fields to their default values
+   */
+  function reset_hour_fields() {
+    my_fire_set_hour_of_arrival("00:00");
+    my_fire_set_pause_duration("00:45");
+    my_fire_set_hour_of_departure("00:00");
+    for (let project = 0; project < get_number_of_projects(); project++) {
+      my_fire_set_project_hours(project + 1, "00:00");
+    }
+  }
+
+  /**
+   * Calculates the amount of projects - minus the IC
+   */
+  function get_number_of_projects() {
+    return document.querySelectorAll(".affectation").length - 1;
+  }
+
+  browser.runtime.onMessage.addListener((message) => {
+    switch (message.command) {
+      case "RA::resetAllInputs":
+        reset_hour_fields();
+        break;
+      case "RA::fillOfficeHours":
+        my_fire_set_hour_of_arrival(message.startOfDay);
+        my_fire_set_pause_duration(message.pause);
+        my_fire_set_hour_of_departure(message.endOfDay);
+        break;
+
+      case "RA::fillProjectHours":
+        const { projectIdx, timeWorked } = message;
+        my_fire_set_project_hours(projectIdx, timeWorked);
+        break;
+      case "prj::getAll":
+        const projects = Array.from(document.querySelectorAll(".titreProj>span")).map(
+          (e) => e.firstChild.nodeValue
+        );
+        console.log(projects);
+
+        return Promise.resolve(projects);
+    }
+  });
 })();
--- popup/choose_hours.js
+++ popup/choose_hours.js
@@ -1,46 +1,138 @@
-
 /**
  * Listen for clicks on the buttons, and send the appropriate message to
  * the content script in the page.
  */
 function listenForClicks() {
-    document.addEventListener("click", (e) => {
-
-      
-        function reset(tabs) {
-            console.log("resetting");
-
-            browser.tabs.sendMessage(tabs[0].id, {
-                command: "reset",
-            }
-            ).catch(reportExecuteScriptError);
-        }
-
-        function fill(tabs) {
-            console.log("filling");
-            browser.tabs.sendMessage(tabs[0].id, {
-                command: "fill",
-                startOfDay: "09:00",
-                endOfDay: "18:00"
-            }).catch(reportExecuteScriptError);;
-        }
-
-
-        if (e.target.tagName !== "BUTTON") {
-            // Ignore when click is not on a button within <div id="popup-content">.
-            return;
-        }
-        if (e.target.type === "reset") {
-            browser.tabs.query({ active: true, currentWindow: true })
-                .then(reset)
-                .catch(reportExecuteScriptError);
-        } else {
-            browser.tabs.query({ active: true, currentWindow: true })
-                .then(fill)
-                .catch(reportExecuteScriptError);
-
-        }
+  // install event listener for local storage of time values in the popup
+  document.querySelectorAll(".timeInput").forEach((input) => {
+    input.addEventListener("keyup", (e) => {
+      let value = {};
+      value[e.target.id] = e.target.value;
+      browser.storage.local.set(value).catch(reportExecuteScriptError);
     });
+  });
+
+  // retreive saved values from the local storage
+  browser.storage.local
+    .get(["startDay", "endDay", "pause", "projectHours"])
+    .then((o) => {
+      // populate the inputs
+      for (const [k, v] of Object.entries(o)) {
+        document.querySelector("#" + k).value = v;
+      }
+    })
+    .catch(reportExecuteScriptError);
+
+  document.addEventListener("click", (e) => {
+    /**
+     * Resets all text fields in the RA
+     * @param {*} tabs the tabs given by the browser [0] is the active one
+     */
+    function reset(tabs) {
+      browser.tabs
+        .sendMessage(tabs[0].id, {
+          command: "RA::resetAllInputs",
+        })
+        .catch(reportExecuteScriptError);
+    }
+
+    /**
+     * Sets the office hours to the given times of arrival and departures
+     * @param {*} tabs the tabs given by the browser [0] is the active one
+     */
+    function fillOfficeHours(tabs) {
+      const start_hour = new MfTime(document.querySelector("#startDay").value);
+      const end_hour = new MfTime(document.querySelector("#endDay").value);
+      const pause_time = new MfTime(document.querySelector("#pause").value);
+
+      if (start_hour.valid && end_hour.valid && pause_time.valid) {
+        browser.tabs
+          .sendMessage(tabs[0].id, {
+            command: "RA::fillOfficeHours",
+            startOfDay: start_hour.mf_time,
+            pauseTime: pause_time.mf_time,
+            endOfDay: end_hour.mf_time,
+          })
+          .catch(reportExecuteScriptError);
+      } else {
+        // TODO:handle validation error
+        console.error(start_hour.raw_input, end_hour.raw_input);
+      }
+    }
+
+    /**
+     * Fills the project hours per project from the selected project
+     * @param {*} tabs the tabs given by the browser [0] is the active one
+     */
+    function fillProjectHours(tabs) {
+      const projectIdx = document.querySelector("#projects").value;
+      const hoursWorked = new MfTime(document.querySelector("#projectHours").value);
+      if (hoursWorked.valid) {
+        browser.tabs
+          .sendMessage(tabs[0].id, {
+            command: "RA::fillProjectHours",
+            projectIdx: projectIdx,
+            timeWorked: hoursWorked.mf_time,
+          })
+          .catch(reportExecuteScriptError);
+      } else {
+        // TODO:handle validation error
+        console.error(start_hour.raw_input, end_hour.raw_input);
+      }
+    }
+    if (e.target.tagName !== "BUTTON") {
+      // Ignore when click is not on a button within <div id="popup-content">.
+      return;
+    }
+
+    switch (e.target.id) {
+      case "reset":
+        browser.tabs
+          .query({ active: true, currentWindow: true })
+          .then(reset)
+          .catch(reportExecuteScriptError);
+        break;
+      case "setHours":
+        browser.tabs
+          .query({ active: true, currentWindow: true })
+          .then(fillOfficeHours)
+          .catch(reportExecuteScriptError);
+        break;
+      case "setProjectHours":
+        browser.tabs
+          .query({ active: true, currentWindow: true })
+          .then(fillProjectHours)
+          .catch(reportExecuteScriptError);
+        break;
+      case "setProjectHours":
+        browser.tabs
+          .query({ active: true, currentWindow: true })
+          .then(fillOfficeHours)
+          .catch(reportExecuteScriptError);
+        break;
+    }
+  });
+}
+
+function populateSelectWithProjects() {
+  browser.tabs
+    .query({ active: true, currentWindow: true })
+    .then((tabs) => {
+      browser.tabs
+        .sendMessage(tabs[0].id, { command: "prj::getAll" })
+        .then((response) => {
+          console.log(response);
+          let prjSel = document.querySelector("#projects");
+          for (const [index, project] of response.entries()) {
+            prjSel.appendChild(new Option(project, index));
+          }
+          if (response.length > 1) {
+            prjSel.value = 1;
+          }
+        })
+        .catch(reportExecuteScriptError);
+    })
+    .catch(reportExecuteScriptError);
 }
 
 /**
@@ -48,9 +140,9 @@ function listenForClicks() {
  * Display the popup's error message, and hide the normal UI.
  */
 function reportExecuteScriptError(error) {
-    document.querySelector("#popup-content").classList.add("hidden");
-    document.querySelector("#error-content").classList.remove("hidden");
-    console.error(`Failed to execute beastify content script: ${error.message}`);
+  document.querySelector("#popup-content").classList.add("hidden");
+  document.querySelector("#error-content").classList.remove("hidden");
+  console.error(`Failed to execute beastify content script: ${error.message}`);
 }
 
 /**
@@ -58,6 +150,8 @@ function reportExecuteScriptError(error) {
  * and add a click handler.
  * If we couldn't inject the script, handle the error.
  */
-browser.tabs.executeScript({ file: "/content_scripts/hourfiller.js" })
-    .then(listenForClicks)
-    .catch(reportExecuteScriptError);
+browser.tabs
+  .executeScript({ file: "/content_scripts/hourfiller.js" })
+  .then(listenForClicks)
+  .then(populateSelectWithProjects)
+  .catch(reportExecuteScriptError);
